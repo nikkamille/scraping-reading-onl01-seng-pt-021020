f4816faa2c764808be422ebf81e5532628bcba4d
Kamille Longalong <k.pagibigan@gmail.com>
2020-03-31 05:28:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,4 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
-doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
