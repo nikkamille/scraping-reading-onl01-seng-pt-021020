489f64f5725d66c033bbaa9a5385ad08b0416e80
Kamille Longalong <k.pagibigan@gmail.com>
2020-03-31 05:20:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
 
-
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
 doc.css(".headline-26OIBN").text 
